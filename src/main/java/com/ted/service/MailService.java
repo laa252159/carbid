6a609ad08d3532fa1a37feef66ae9cfc07d3e3b7
Alexander Lobov <Aleksandr_Lobov@epam.com>
2019-07-23 22:51:32 +0300

    mail cars64

--- src/main/java/com/ted/service/MailService.java
+++ src/main/java/com/ted/service/MailService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface MailService {
 
-    String SENDER = "noreply@perekup64.ru";
+    String SENDER = "noreply@cars64.ru";
 
     String[] admins_emails = {
             "laa252159@gmail.com",
